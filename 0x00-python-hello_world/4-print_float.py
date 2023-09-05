68c2130980e17f5dc801fabfd5c36563ff17558a
Abdullah Mohammed <abdullahmohammed1am@gmail.com>
2023-09-05 01:29:58 -0700

    python

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 number = 3.14159
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-
-print(f"Float: {number:.2f}"))
+formatted_number = f"{number:.2f}"
+print(f"Float: {formatted_number}\n")
