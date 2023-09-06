ae2cfdce36127179621bd152c7f07f544c6b4b54
Abdullah Mohammed <abdullahmohammed1am.com>
2023-09-06 19:57:22 +0000

    python

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,6 @@
-name = 'a'
-while name <= 'z':
-    print(name , end = '')
-    name = chr(ord(name) + 1)
+#!/usr/bin/python3
+# 2-print_alphabet.py
+
+"""Print the alphabet in lowercase, not followed by a new line."""
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
