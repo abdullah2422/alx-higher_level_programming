5de7cda9662a10e815ad4e34e54e5dc3b15aa427
Abdullah Mohammed <abdullahmohammed1am.com>
2023-09-07 08:45:35 +0000

    python1

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i < 99:
-        print(f"{str(i).zfill(2)}" , end = ', ')
-    else:
-        print(f"{str(i).zfill(2)}" , end = ' ')
+for i in range(99):
+    print("{:02d}".format(i), end=", ")
+print("{:02d}".format(99))
