3f0d9281a54a4745622a6a6f080e9fd11160682e
Abdullah Mohammed <abdullahmohammed1am.com>
2023-09-11 17:18:13 +0000

    python3

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,6 +3,6 @@
 
 def replace_in_list(my_list, idx, element):
     """replaces an element of a list at a specific position (like in C)."""
-    if idx >= 0 or idx >= len(my_list):
+    if idx >= 0 and idx >= len(my_list):
         my_list[idx] = element
     return (my_list)
