44473c32a5bd46821545005526bbd729dfc6d09b
Abdullah Mohammed <abdullahmohammed1am.com>
2023-09-11 19:34:38 +0000

    python

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 3-print_reversed_list_integer.py
+def print_reversed_list_integer(my_list=[]):
+    """Print all integers of a list in reverse order."""
+    list = len(my_list)
+    for i in range (list ,0, -1):
+        print(i)
