2ce2f1e9e8ef09f0a5bbf61e20e99f1e9a5fee21
Abdullah Mohammed <abdullahmohammed1am.com>
2023-09-11 22:41:42 +0000

    python

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 # 4-new_in_list.py
-
-
 def new_in_list(my_list, idx, element):
-    """Replace an element in a copied list at a specific position."""
-    if idx < 0 or idx > (len(my_list) - 1):
-        return (my_list)
-
-    copy = [x for x in my_list]
-    copy[idx] = element
-    return (copy)
+    """Replace an element in a copied list at a specific position."""    my_list = my_list.copy()
+    if idx < 0 and idx > (len(my_list)):
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
