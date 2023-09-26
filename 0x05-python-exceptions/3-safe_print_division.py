f04935f4a98dfc907fb31cdafad9e27a0ac1394e
abdullah2422 <abdullahmohammed1am@gmail.com>
2023-09-26 02:44:00 +0000

    python5

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """
+    Returns the division of a by b.
+    """
+
+    try:
+        result = a/b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
