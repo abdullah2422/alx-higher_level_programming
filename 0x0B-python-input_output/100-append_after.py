6ed575a464683d8d3d847d90e6453b1d14fd696d
abdullah2422 <abdullahmohammed1am@gmail.com>
2023-10-11 02:05:44 +0000

    pythonB

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,16 +3,11 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            lines = file.readlines()
+    with open(filename, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
 
-        with open(filename, 'w', encoding='utf-8') as file:
-            for line in lines:
-                file.write(line)
-                if search_string in line:
-                    file.write(new_string)
-
-# Usage in your script:
-if __name__ == '__main__':
-    append_after("append_after_100.txt", "Python", "\"C is fun!\"\n")
+    with open(filename, 'w', encoding='utf-8') as file:
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string)
