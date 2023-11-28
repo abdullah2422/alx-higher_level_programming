2aaa891f5a6da5d31844e78b6cc8bb2626fb75dc
abdullah2422 <abdullahmohammed1am@gmail.com>
2023-11-28 14:23:02 +0000

    python

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -6,12 +6,12 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        
+
     @property
     def width(self):
         """Retrieves the width of the Rectangle instance."""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """Sets the width of the Rectangle instance."""
@@ -25,7 +25,7 @@ class Rectangle:
     def height(self):
         """Retrieves the height of the Rectangle instance."""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """Retrieves the height of the Rectangle instance."""
@@ -35,12 +35,11 @@ class Rectangle:
              raise ValueError("with the message height must be >= 0")
         else:
             self.__height = value
-            
+
     def area(self):
         """Returns the area of the Rectangle instance."""
         return self.width * self.height
-  
+
     def perimeter(self):
         """Returns the perimeter of the Rectangle instance."""
-        return 2 * (self.width + self.height) \
-            if self.width != 0 and self.height != 0 else 0
+        return 2 * (self.width + self.height) if self.width != 0 and self.height != 0 else 0
